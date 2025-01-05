442d36260b8182f85577364211ed1b2410260a94
jay-bennett-JB <jaydbennett92@gmail.com>
2025-01-05 15:53:35 +0000

    new file:   fastAPI/database.py 	new file:   fastAPI/main.py 	new file:   fastAPI/models.py

---
+++ fastAPI/database.py
@@ -0,0 +1,8 @@
+# Imports
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+URL_DATABASE ='sqlite:///./taskDatabase.db'
+
+engine = create_engine(URL_DATABASE, connect_args={"check_same_thread": False})
