718210a22881203f4843110de6a62ea2e0dc4730
jay-bennett-JB <jaydbennett92@gmail.com>
2025-01-06 20:13:04 +0000

    new file:   React/frontapp/.gitignore 	new file:   React/frontapp/README.md 	new file:   React/frontapp/package-lock.json 	new file:   React/frontapp/package.json 	new file:   React/frontapp/public/favicon.ico 	new file:   React/frontapp/public/index.html 	new file:   React/frontapp/public/logo192.png 	new file:   React/frontapp/public/logo512.png 	new file:   React/frontapp/public/manifest.json 	new file:   React/frontapp/public/robots.txt 	new file:   React/frontapp/src/App.js 	new file:   React/frontapp/src/index.css 	new file:   React/frontapp/src/index.js 	new file:   fastAPI/__pycache__/database.cpython-313.pyc 	new file:   fastAPI/__pycache__/main.cpython-313.pyc 	new file:   fastAPI/__pycache__/models.cpython-313.pyc 	modified:   fastAPI/database.py 	modified:   fastAPI/main.py 	modified:   fastAPI/models.py

---
+++ fastAPI/main.py
@@ -0,0 +1,73 @@
+# Imports
+from fastapi import FastAPI, HTTPException, Depends
+from typing import Annotated
+from sqlalchemy.orm import Session
+from pydantic import BaseModel
+from .database import SessionLocal, engine
+from . import models
+from fastapi.middleware.cors import CORSMiddleware
+from datetime import datetime
+
+# Api Setup
+app = FastAPI()
+
+# Origin Declaration
+origins = ["http://localhost:3000"]
+
+# Middleware declaration
+# this allows for a request from only origin declared values, in addition to restricting request types
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    # allow_methods=["GET", "POST", "PUT", "DELETE"],
+    # allow_headers=["*"],
+)
+
+
+# TransactionBase Class - Using pydantic BaseModel inheritance to valid data received from React frontend
+class TransactionBase(BaseModel):
+    name: str
+    description: str
+    dueDate: datetime
+    priority: bool
+
+
+# Transaction model - this declares the ID field and orm_mode to True
+class TransactionModel(TransactionBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+# Database Connection function, this will try the database with a request
+# if request is invalid the database will be closed, in addition to when request is completed
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+# Database dependency
+db_dependency = Annotated[Session, Depends(get_db)]
+
+models.Base.metadata.create_all(bind=engine)
+
+
+# Post function - maps all values into  transaction table created in sqlite
+@app.post("/transactions/", response_model=TransactionModel)
+async def create_transaction(transaction: TransactionBase, db: db_dependency):
+    db_transaction = models.Transaction(**transaction.dict())
+    db.add(db_transaction)
+    db.commit()
+    db.refresh(db_transaction)
+    return db_transaction
+
+
+# Get Function
+@app.get("/transactions/", response_model=[TransactionModel])
+async def read_transactions(db: db_dependency, skip: int = 0, limit: int = 100):
+    transactions = db.query(models.Transaction).offset(skip).limit(limit).all()
+    return transactions
---
+++ fastAPI/models.py
@@ -0,0 +1,16 @@
+# Imports
+from sqlalchemy import Column, Integer, String, Boolean, Float, Date, DateTime
+from .database import Base
+
+
+# Table Class
+class Transaction(Base):
+    # Table name
+    __tablename__ = "transactions"
+
+    # Column Set up
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    description = Column(String)
+    dueDate = Column(DateTime)
+    priority = Column(Boolean, nullable=False, default=False)
